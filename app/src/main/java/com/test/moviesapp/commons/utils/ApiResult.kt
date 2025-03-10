aedfcde653d63f760b267ba454f96890ce7a461a
Alejandra Toledano <toledanomarino@gmail.com>
2025-03-09 18:54:11 -0600

    feat: add custom message for errors

--- app/src/main/java/com/test/moviesapp/commons/utils/ApiResult.kt
+++ app/src/main/java/com/test/moviesapp/commons/utils/ApiResult.kt
@@ -5,5 +5,5 @@ import com.test.moviesapp.domain.ErrorDomainModel
 sealed class ApiResult<out T> {
     data class Loading(val isLoading: Boolean) : ApiResult<Nothing>()
     data class Success<T>(val data: T) : ApiResult<T>()
-    data class Error(val error: ErrorDomainModel) : ApiResult<Nothing>()
+    data class Error<T>(val error: ErrorDomainModel) : ApiResult<T>()
 }
--- app/src/main/java/com/test/moviesapp/domain/ErrorDomainModel.kt
+++ app/src/main/java/com/test/moviesapp/domain/ErrorDomainModel.kt
@@ -1,7 +1,36 @@
 package com.test.moviesapp.domain
 
+import android.content.Context
+import com.test.moviesapp.R
+
 sealed class ErrorDomainModel {
     data class ApiError(val code: Int, val message: String) : ErrorDomainModel()
     data object NetworkError : ErrorDomainModel()
     data object UnknownError : ErrorDomainModel()
+
+    fun getMessage(context: Context): String {
+        return when (this) {
+            is NetworkError -> context.getString(R.string.error_network)
+            is UnknownError -> String.format(
+                context.getString(R.string.error_generic),
+                "UNKNOWN",
+                context.getString(R.string.error_generic_message)
+            )
+            is ApiError -> {
+                when (code) {
+                    // Take from https://developer.themoviedb.org/docs/errors
+                    429 -> String.format(
+                        context.getString(R.string.error_generic),
+                        code.toString(),
+                        context.getString(R.string.error_request_count)
+                    )
+                    else -> String.format(
+                        context.getString(R.string.error_generic),
+                        code.toString(),
+                        context.getString(R.string.error_generic_message)
+                    )
+                }
+            }
+        }
+    }
 }
